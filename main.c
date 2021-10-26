cf64141a549688ddc30963f977730be86dcaad40
Rick Mark <rickmark@outlook.com>
2021-10-26 11:08:39 -0700

    Fix for hang at end

--- main.c
+++ main.c
@@ -5,8 +5,6 @@
 #include <unistd.h>
 #include <dlfcn.h>
 
-static volatile bool finished = false;
-
 typedef int (*extract_function)(const char* shared_cache_file_path, const char* extraction_root_path, void (^progress)(unsigned current, unsigned total));
 
 int main(int argc, char* argv[]) {
@@ -19,18 +17,14 @@ int main(int argc, char* argv[]) {
 
     void (^callback)(unsigned current, unsigned total) = ^(unsigned int current, unsigned int total) {
         printf("Progress: %d/%d\n", current, total);
-
-        if (current == total) {
-            finished = true;
-        }
     };
 
-    extract(argv[1], argv[2], callback);
+    int result = extract(argv[1], argv[2], callback);
 
-    while (finished == false) {
-        sleep(1000);
+    if (result) {
+        printf("Failed to call extract (error %d)\n", result);
+        exit(-1);
     }
-
     printf("Finished!\n");
     return 0;
 }
